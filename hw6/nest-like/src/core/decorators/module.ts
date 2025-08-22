467a1437efaa8bdc8e23da262168e3783a19d506
DimaDzh <dzhartyds@gmail.com>
2025-08-22 10:26:49 +0300

    added imports to module, decrotors to route

--- hw6/nest-like/src/core/decorators/module.ts
+++ hw6/nest-like/src/core/decorators/module.ts
@@ -1,4 +1,8 @@
-export function Module(metadata: { controllers?: any[]; providers?: any[] }) {
+export function Module(metadata: {
+  imports?: any[];
+  controllers?: any[];
+  providers?: any[];
+}) {
   return function (target: any) {
     Reflect.defineMetadata("mini:module", metadata, target);
   };
--- hw6/nest-like/src/core/decorators/route.ts
+++ hw6/nest-like/src/core/decorators/route.ts
@@ -12,3 +12,5 @@ export function Route(method: Method, path = "") {
 /* sugar helpers */
 export const Get = (p = "") => Route("get", p);
 export const Post = (p = "") => Route("post", p);
+export const Put = (p = "") => Route("put", p);
+export const Delete = (p = "") => Route("delete", p);
